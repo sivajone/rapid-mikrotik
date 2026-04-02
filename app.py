30a773a83b1d8ad010ffb8ed70fc1f5ab9e7dfdb
sivajone <sivajone@protonmail.com>
2026-04-02 02:30:34 +0200

    use turboflask

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-import os
-from dotenv import load_dotenv
+from flask import Flask, render_template
 import requests
-from flask import Flask
+from turbo_flask import Turbo
+import threading
+import time
+
 app = Flask(__name__)
+turbo = Turbo(app)
 
-load_dotenv()
-url = os.environ.get("URL")
-user = os.environ.get("USER")
-passwd = os.environ.get("PASS")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-print(url)
+@app.context_processor
+def inject_data():
+    r = requests.get("https://timeapi.io/api/v1/time/current/unix")
+    data = r.json()
+    return {'data': data}
 
-@app.route('/api/devices')
-def get_data():
-    r = requests.get(url, auth=(user, passwd), verify=False)
-    return r.json()
+@app.before_request
+def before_first_request():
+    threading.Thread(target=update_data).start()
 
+def update_data():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.replace(render_template('index.html'), 'data'))
